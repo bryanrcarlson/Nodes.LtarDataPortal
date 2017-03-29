4a7d03bdb765324e1221c9470caaeff58bf5fbfd
Bryan Carlson <bryan.r.carlson@gmail.com>
2017-03-29 10:08:58 -0700

    Init

---
+++ MetWriter/Codes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetWriter
+{
+    public static class LtarSiteAcronymCodes
+    {
+        // TODO: Incomplete
+        public const string CookAgronomyFarm = "CAF";
+    }
+
+    public static class RecordTypeCodes
+    {
+        public const string LegacySiteSpecificDefinition = "L";
+        public const string CommonObservationProtocol = "C";
+    }
+}
---
+++ MetWriter/DataRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nsar.Nodes.CORe.MetWriter
+{
+    public class DataRecord
+    {
+        public string LTARSiteAcronym { get; set; }
+        public string StationID { get; set; }
+        public string DateTime { get; set; }
+        public string RecordType { get; set; }
+        public string AirTemperature { get; set; }
+        public string WindSpeed { get; set; }
+        public string WindDirection { get; set; }
+        public string RelativeHumidity { get; set; }
+        public string Precipitation { get; set; }
+        public string AirPressure { get; set; }
+        public string PAR { get; set; }
+        public string ShortWaveIn { get; set; }
+        public string LongWaveIn { get; set; }
+        public string BatteryVoltage { get; set; }
+        public string LoggerTemperature { get; set; }
+    }
+}
